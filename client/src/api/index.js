f42f9ea9e272237ad7ea189ea191e3154f24c456
SUDHA KUMARI GURUNG <85292259+sudha8181@users.noreply.github.com>
2024-01-06 18:57:14 +0530

    Update api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API=axios.create({baseURL:`http://localhost:5500/`})
+const API=axios.create({baseURL:`https://youtubeclone8034.onrender.com`})
 API.interceptors.request.use(req=>{
     if(localStorage.getItem('Profile')){
         req.headers.authorization=`Bearer ${JSON.parse(localStorage.getItem('Profile')).token}`
